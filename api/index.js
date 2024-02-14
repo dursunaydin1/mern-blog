d2355b564595820db2c77397f2a7899716b72d39
dursunaydin1 <dursunayydin@gmail.com>
2024-02-14 17:46:45 +0300

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,11 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL || "mongodb://localhost:27017/mern-blog")
+  .then(() => {
+    console.log("MongoDB bağlantısı başarıyla sağlandı...");
+  })
+  .catch((err) => {
+    console.error("MongoDB bağlantısı sağlanırken bir hata oluştu:", err);
+  });
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
 app.listen(3000, () => {
-  console.log("Server 3000 portunda çalışıyor...");
+  console.log("API sunucusu 3000 numaralı bağlantı noktasında çalışıyor...");
 });
