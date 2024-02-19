a5b70b41261717e95fce7f83ffe68a8f588b0c54
dursunaydin1 <dursunayydin@gmail.com>
2024-02-19 17:39:06 +0300

    create get users API route

--- REDME.md
+++ REDME.md
@@ -38,4 +38,5 @@
 38-add show more functionality to the posts results of the dashboard
 39-add delete post functionality to the dashboard
 40-add update post functionality
-41-
+41-create get users API route
+42-
--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -78,3 +78,47 @@ export const signout = async (req, res, next) => {
     next(error);
   }
 };
+
+// Kullanıcı bilgilerini getir
+export const getUsers = async (req, res, next) => {
+  if (!req.user.isAdmin) {
+    return next(
+      errorHandler(403, "Bu kullanıcı bilgilerini getirme yetkiniz yok")
+    );
+  }
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.sort === "asc" ? 1 : -1;
+    const users = await User.find({})
+      .sort({ updatedAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+    const userWithoutPassword = users.map((user) => {
+      const { password, ...rest } = user._doc;
+      return rest;
+    });
+
+    const totalUsers = await User.countDocuments({});
+
+    const now = new Date();
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+
+    const lastMonthUsers = await User.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+
+    res.status(200).json({
+      users: userWithoutPassword,
+      totalUsers,
+      lastMonthUsers,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
